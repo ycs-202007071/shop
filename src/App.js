27de111cf7b4b697edabdd589fabba19d7c13e3b
Eun Ji <kws03337@naver.com>
2024-12-11 17:55:59 +0900

    12.11

--- src/App.js
+++ src/App.js
@@ -9,23 +9,24 @@ import axios from 'axios'
 
 function App() {
 
-  let [shoes] = useState(data)
+  let [shoes, setShoes] = useState(data);
   let navigate = useNavigate();
+  let [count, setCount] = useState(0);
+
 
   return (
     <div>
-
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-            <Nav.Link onClick={()=>{navigate('./detail')}}>detail</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('./detail') }}>detail</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      
+
       <Routes>
         <Route path="/" element={
           <>
@@ -35,18 +36,17 @@ function App() {
               <div className="row">
                 {
                   shoes.map((a, i) => {
-                    return <Card shoes={shoes} num={i} />
+                    return <Card shoes={shoes} num={i} key={i} />
                   })
                 }
               </div>
             </div>
-            <button onClick={()=>{
-
-            }}>버튼</button>
+            {count<2 ?  <PlusButton shoes={shoes} count={count} setShoes={setShoes} setCount={setCount}/> :null}
+           
           </>
         } />
-        <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />//:id=url파라미터
-        <Route path="/about" element={<About/>} >
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />//:id=url파라미터
+        <Route path="/about" element={<About />} >
           <Route path="member" element={<div>멤버임</div>} />
           <Route path="location" element={<div>위치정보임</div>} />
         </Route>
@@ -55,7 +55,7 @@ function App() {
           <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
         </Route>
 
-        
+
         <Route path="*" element={<div>없는 페이지에용</div>} />
       </Routes>
 
@@ -66,8 +66,8 @@ function App() {
 }
 
 
-function About(){
-  return(
+function About() {
+  return (
     <div>
       <h4>회사정보</h4>
       <Outlet></Outlet>
@@ -79,12 +79,41 @@ function About(){
 function Card(props) {
   return (
     <div className="col-md-4">
-      <Link to={"/detail/"+(props.num)}>
-      <img src={"https://codingapple1.github.io/shop/shoes" + (props.num + 1) + ".jpg"} width="80%" />
+      <Link to={"/detail/" + (props.num)}>
+        <img src={"https://codingapple1.github.io/shop/shoes" + (props.num + 1) + ".jpg"} width="80%" />
       </Link>
       <h4>{props.shoes[props.num].title}</h4>
       <h4>{props.shoes[props.num].price}</h4>
     </div>
+  )
+}
+
+function PlusButton(props){
+  return(
+    <button onClick={() => {
+      if (props.count === 0) {
+        axios.get('https://codingapple1.github.io/shop/data2.json')
+          .then((결과) => {
+            let copy = [...props.shoes, ...결과.data]
+            props.setShoes(copy);
+            props.setCount(1);
+          })
+          .catch(() => {
+
+          })
+      }else if (props.count === 1) {
+        axios.get('https://codingapple1.github.io/shop/data3.json')
+          .then((결과) => {
+            let copy = [...props.shoes, ...결과.data]
+            props.setShoes(copy);
+            props.setCount(2);
+          })
+          .catch(() => {
+
+          })
+      }
+
+    }}>버튼</button>
   )
 }
 
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,51 +1,77 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import {Nav} from 'react-bootstrap';
 
 function Detail(props) {
   let [num, setNUm] = useState('')
-  useEffect(()=>{
-    if(isNaN(num) == true){
+  useEffect(() => {
+    if (isNaN(num) == true) {
       alert("숫자만 입력해주세요")
     }
-    let time = setTimeout(()=>{setShowAlert(false)}, 2000)
-    return()=>{
+    let time = setTimeout(() => { setShowAlert(false) }, 2000)
+    return () => {
       clearTimeout(time);
       //unmount시 실행됨.
       //useEffect 실행전에 실행할 코드는 여기에 적으면 됨. clean up function 타이머 사용시 기존 타이머는 제거해주세요 , 기존 데이터요청은 제거해주세요. 등으로 사용
     }
   }, [num])//[]를 붙이면 mount때만 실행되고 update때는 실행이 되지 않음
-  
-  
+
+
   let [count, setCount] = useState(0)
   let [showAlert, setShowAlert] = useState(true)
+  let [tap, setTap] = useState(0)
 
 
-  let {id} = useParams();
-  let result = props.shoes.find((x) => x.id == id )
+  let { id } = useParams();
+  let result = props.shoes.find((x) => x.id == id)
 
-    return (
-      <div className="container">
-        {
-          showAlert == true 
-          ?  <div className ="alert alert-warning">
-          2초이내 구매시 할인
-        </div> : null
-}
-        <button onClick={()=>{setCount(count+1)}}>버튼</button>
-        <div className="row">
-          <div className="col-md-6">
+  return (
+    <div className="container">
+      {
+        showAlert == true
+          ? <div className="alert alert-warning">
+            2초이내 구매시 할인
+          </div> : null
+      }
+      <button onClick={() => { setCount(count + 1) }}>버튼</button>
+      <div className="row">
+        <div className="col-md-6">
           <img src={"https://codingapple1.github.io/shop/shoes" + (result.id + 1) + ".jpg"} width="100%" />
-          </div>
-          <input onChange={(e)=>{setNUm(e.target.value)}}></input>
-          <div className="col-md-6">
-            <h4 className="pt-5">{result.title}</h4>
-            <p>{result.content}</p>
-            <p>{result.price}</p>
-            <button className="btn btn-danger">주문하기</button>
-          </div>
+        </div>
+        <input onChange={(e) => { setNUm(e.target.value) }}></input>
+        <div className="col-md-6">
+          <h4 className="pt-5">{result.title}</h4>
+          <p>{result.content}</p>
+          <p>{result.price}</p>
+          <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
-    )
-  }
 
-  export default Detail;
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link eventKey="link0" onClick={setTap(0)}>버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link1" onClick={setTap(1)}>버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link2" onClick={setTap(2)}>버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TapContent tap={tap}></TapContent>
+    </div>
+  )
+}
+function TapContent(props){
+  if(props.tap==0){
+    return <div>내용0</div>
+  }
+  else if(props.tap==1){
+    return <div>내용1</div>
+  }
+  else if(props.tap==2){
+    return <div>내용2</div>
+  }
+}
+
+export default Detail;
